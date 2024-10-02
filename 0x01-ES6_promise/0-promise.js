da02b00fdf399c00fa922deb0a0e8c32edf1b081
voste126 <steveaustine126@gmail.com>
2024-10-02 14:15:28 +0000

    FIX:adding comments to code

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,5 @@
-/**
- * This function returns a promise that simulates an API response.
- * It creates a new Promise object and immediately resolves it, 
- * simulating a successful API call. You can add additional logic 
- * inside the promise if needed.
- */
-
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    // You can add some logic here if needed.
     resolve();
   });
 }
